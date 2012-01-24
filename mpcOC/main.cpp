863dcef372650b891c2b4123504cc72c063c2420
berezhnoy <berezhnoy@5079a5ec-52b0-490e-b3bd-9f0da412a43b>
2012-01-24 07:11:41 +0000

    mpcOC
    git-svn-id: svn+ssh://195.201.30.214/var/svn-repos/lazaProj/branches/berBranch/trunk@231 5079a5ec-52b0-490e-b3bd-9f0da412a43b

---
+++ mpcOC/main.cpp
@@ -0,0 +1,90 @@
+#include <QtCore/QCoreApplication>
+#include "./../libs/fitsdata.h"
+#include "./../libs/redStat.h"
+#include <QString>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    objResidualFile orFile;
+    eqFile eqResFile;
+    objResRec *orTemp;
+    ocRec *ocTemp;
+    mpephRec mpeRec;
+    QString fileName = QString(argv[1]);
+
+    orFile.init(fileName);
+    QFileInfo fi(fileName);
+    QString oFileName;
+    if(argc>2)
+    {
+        oFileName = QString(argv[2]);
+    }
+    else
+    {
+        oFileName = QString("%1/%2_eq.txt").arg(fi.absolutePath()).arg(fi.completeBaseName());
+    }
+
+    QString cfgFileName = "mpcOC.ini";
+
+    procData mpephProcData;
+    procData miriProcData;
+
+    QSettings *sett = new QSettings(cfgFileName, QSettings::IniFormat);
+
+//objects   ////
+    //int lspmFind = sett->value("objects/lspmFind", 0).toInt();
+    //int skybotFind = sett->value("objects/skybotFind", 0).toInt();
+    //int tryMpeph = sett->value("general/tryMpeph", 0).toInt();
+    int useMiriade = sett->value("general/useMiriade", 0).toInt();
+    //int mpephType = sett->value("objects/mpephType", 0).toInt();
+    //double magObj0 = sett->value("objects/mag0", 6.0).toDouble();
+    //double magObj1 = sett->value("objects/mag1", 16.0).toDouble();
+    //QString headObjName = sett->value("objects/headObjName", "OBJECT").toString();
+
+    mpephProcData.name = sett->value("processes/mpeph_prog", "./mpeph").toString();
+    mpephProcData.folder = sett->value("processes/mpeph_prog_folder", "./").toString();
+    mpephProcData.waitTime = sett->value("processes/mpeph_wait_time", -1).toInt();
+    if(mpephProcData.waitTime>0) mpephProcData.waitTime *= 1000;
+    miriProcData.name = sett->value("processes/miriade_prog", "./miriadeEph").toString();
+    miriProcData.folder = sett->value("processes/miriade_prog_folder", "./").toString();
+    miriProcData.waitTime = sett->value("processes/miriade_wait_time", -1).toInt();
+    if(miriProcData.waitTime>0) miriProcData.waitTime *= 1000;
+
+    int i, sz, getRes;
+    QString tStr;
+    sz = orFile.ocList.size();
+
+    for(i=0; i<sz; i++)
+    {
+        orTemp = orFile.ocList.at(i);
+        if(useMiriade) getRes = getMiriadeObject(&mpeRec, orTemp->mJD, orTemp->name, miriProcData);
+        else getRes = getMpephObject(&mpeRec, orTemp->mJD, orTemp->name, 1, mpephProcData);
+
+        if(!getRes)
+        {
+            ocTemp = new ocRec;
+            orTemp->toOcRec(ocTemp);
+            ocTemp->ocRaCosDe = grad_to_mas(orTemp->ra*cos(grad2rad(orTemp->de)) - mpeRec.ra*cos(grad2rad(mpeRec.de)));
+            ocTemp->ocDe = grad_to_mas(orTemp->de - mpeRec.de);
+            ocTemp->elong = mpeRec.elong;
+            ocTemp->ocMag = orTemp->mag - mpeRec.Vmag;
+            ocTemp->phase = mpeRec.phase;
+            ocTemp->Vr = mpeRec.Vr;
+            ocTemp->muRaCosDe = mpeRec.muRaCosDe;
+            ocTemp->muDe = mpeRec.muDe;
+            ocTemp->topDist = mpeRec.topDist;
+            if(useMiriade) ocTemp->catName = QString("miriade");
+            else ocTemp->catName = QString("mpeph");
+            ocTemp->rec2s(&tStr);
+            qDebug() << tStr << "\n";
+            eqResFile.ocList.append(ocTemp);
+        }
+        else qDebug() << "\ngetMpephObject\n";
+    }
+
+    qDebug() << QString("save %1\n").arg(oFileName);
+    eqResFile.saveAs(oFileName);
+
+    return 0;//a.exec();
+}
---
+++ mpcOC/mpcOC.pro
@@ -0,0 +1,84 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-01-23T13:50:21
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = mpcOC
+
+CONFIG += console \
+    exceptions \
+    warn_off
+
+HEADERS += ../libs/astro.h \
+    ../libs/mb.h \
+    ../libs/fitsio.h \
+    ../libs/fitsdata.h \
+    ../libs/comfunc.h \
+    ../libs/multidim.h \
+    ../libs/ccdmeasurements.h \
+    ../libs/redStat.h \
+    ../libs/observatory.h \
+    ../libs/identify.h \
+    ../libs/cmnk.h \
+    ../libs/sscat.h \
+    ../libs/fBuffer.h \
+    ../libs/vectGrid3D.h \
+    ../libs/fitstools.h\
+    ../libs/alglib/ap.h \
+    ../libs/alglib/interpolation.h \
+    ../libs/alglib/specialfunctions.h \
+    ../libs/alglib/solvers.h \
+    ../libs/alglib/optimization.h \
+    ../libs/alglib/linalg.h \
+    ../libs/alglib/integration.h \
+    ../libs/alglib/alglibmisc.h \
+    ../libs/alglib/alglibinternal.h \
+    ../libs/sysCorr.h \
+    ../libs/mpcs.h \
+    ../libs/mpcqt.h
+
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    ../libs/fitsdata.cpp \
+    ../libs/comfunc.cpp \
+    ../libs/multidim.cpp \
+    ../libs/ccdmeasurements.cpp \
+    ../libs/redStat.cpp \
+    ../libs/observatory.cpp \
+    ../libs/identify.cpp \
+    ../libs/cmnk.cpp \
+    ../libs/sscat.cpp \
+    ../libs/fBuffer.cpp \
+    ../libs/vectGrid3D.cpp \
+    ../libs/fitstools.cpp\
+    ../libs/alglib/ap.cpp \
+    ../libs/alglib/interpolation.cpp \
+    ../libs/alglib/specialfunctions.cpp \
+    ../libs/alglib/solvers.cpp \
+    ../libs/alglib/optimization.cpp \
+    ../libs/alglib/linalg.cpp \
+    ../libs/alglib/integration.cpp \
+    ../libs/alglib/alglibmisc.cpp \
+    ../libs/alglib/alglibinternal.cpp \
+    ../libs/sysCorr.cpp \
+    ../libs/mpcs.cpp \
+    ../libs/mpcqt.cpp
+
+
+win32:LIBS += ./../libs/win32/cfitsio.lib \
+            ./../libs/win32/libmb.a \
+            ./../libs/win32/libastro.a
+unix:LIBS += ./../libs/unix/libcfitsio.a \
+            ./../libs/unix/libmb.a \
+            ./../libs/unix/libastro.a
+
+
+
