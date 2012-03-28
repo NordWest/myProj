ff6bc9233bfa67c50594ada78294b5ab27b1c158
berezhnoy <berezhnoy@5079a5ec-52b0-490e-b3bd-9f0da412a43b>
2012-03-28 11:34:45 +0000

    git-svn-id: svn+ssh://195.201.30.214/var/svn-repos/lazaProj/branches/berBranch/trunk@306 5079a5ec-52b0-490e-b3bd-9f0da412a43b

--- r3StatPL/orStat/main.cpp
+++ r3StatPL/orStat/main.cpp
@@ -1,5 +1,6 @@
 #include <QtCore/QCoreApplication>
 #include "./../../libs/redStat.h"
+#include "./../../mb/mb.h"
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +10,7 @@ int main(int argc, char *argv[])
 
     iniResFile.init(QString(argv[1]));
 
+    iniResFile.do3sigma(0.0, 3.0);
 
     sortORList(&iniResFile.ocList);
     double t0, t1, dt, tt0, tt1, tt;
@@ -17,6 +19,7 @@ int main(int argc, char *argv[])
     sz = iniResFile.ocList.size();
     t0 = iniResFile.ocList.first()->mJD;
     t1 = iniResFile.ocList.last()->mJD;
+    double mjd2000 = 51544.5;
 
     dt = atof(argv[2])*365.2425;
 
@@ -25,6 +28,8 @@ int main(int argc, char *argv[])
     QTextStream resStm(&resFile);
 
     QList <colRec*> cListT;
+    QList <double> lListX, lListY, lListT;
+//    double *x;
 
     k=0;
     tt0 = t0+dt*k;
@@ -41,6 +46,9 @@ int main(int argc, char *argv[])
             if(!tempRF.countCols(&cListT, "0,6,7")&&cListT.at(0)->num>20)
             {
                 resStm << QString("%1|%2|%3|%4|%5|%6|%7|%8\n").arg(cListT.at(0)->mean).arg(cListT.at(0)->num).arg(cListT.at(1)->mean).arg(cListT.at(2)->mean).arg(cListT.at(1)->rmsMean).arg(cListT.at(2)->rmsMean).arg(cListT.at(1)->rmsOne).arg(cListT.at(2)->rmsOne);
+                lListX << pow(cListT.at(1)->rmsOne,2.0);
+                lListY << pow(cListT.at(2)->rmsOne,2.0);
+                lListT << pow(mjd2000-cListT.at(0)->mean, 2.0);
             }
             tempRF.clear();
             cListT.clear();
@@ -51,6 +59,36 @@ int main(int argc, char *argv[])
 
     }
     resFile.close();
+
+////////////////////
+
+    sz = lListX.size();
+
+    double *Lx,*Ly, *Cx, *Cy, *Zx, *Zy;
+    Lx = new double[sz];
+    Ly = new double[sz];
+    Zx = new double[sz];
+    Zy = new double[sz];
+    Cx = new double[sz*2];
+    Cy = new double[sz*2];
+
+    for(i=0; i<sz; i++)
+    {
+        Lx[i] = lListX[i];
+        Ly[i] = lListY[i];
+        Cx[i*2] = 1.0;
+        Cx[i*2+1] = lListT[i];
+        Cy[i*2] = 1.0;
+        Cy[i*2+1] = lListT[i];
+    }
+
+    slsm(2, sz, Zx, Cx, Lx);
+    qDebug() << QString("\nx: %1 mas %2 mas/yr\n").arg(sqrt(fabs(Zx[0]))).arg(sqrt(fabs(Zx[1]))*365.2425);
+
+    slsm(2, sz, Zy, Cy, Ly);
+    qDebug() << QString("y: %1 mas %2 mas/yr\n").arg(sqrt(fabs(Zy[0]))).arg(sqrt(fabs(Zy[1]))*365.2425);
+
+
 
     return 0;//a.exec();
 }
