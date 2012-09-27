6ab0a88764ccb827bd197a0a4d0fb88d6b5982f9
berezhnoy <berezhnoy@5079a5ec-52b0-490e-b3bd-9f0da412a43b>
2012-09-27 19:35:28 +0000

    -
    git-svn-id: svn+ssh://195.201.30.214/var/svn-repos/lazaProj/branches/berBranch/trunk@499 5079a5ec-52b0-490e-b3bd-9f0da412a43b

--- mpcDebias/main.cpp
+++ mpcDebias/main.cpp
@@ -118,7 +118,7 @@ int main(int argc, char *argv[])
                     //rat = asin(0.5*sin(rat)*(rs2-rs1) + 0.5*(rs2+rs1));
                 }*/
 
-                queryStr = QString("SELECT healStore, r0, d0, %1tmN, %1tmMiss, %1tmXbar, %1tmXsig, %1tmYbar, %1tmYsig, %1tmFract, %1tmRadius FROM xcat  WHERE healStore=%2").arg(catPref).arg(ipix);
+                queryStr = QString("SELECT healStore, r0, d0, %1tmN, %1tmMiss, %1tmXbar, %1tmXsig, %1tmYbar, %1tmYsig, %1tmFract, %1tmRadius FROM xcat  WHERE healStore=%2").arg(catPref).arg(i);
 
                 if(!query.exec(queryStr))
                 {
