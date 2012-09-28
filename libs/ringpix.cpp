898bd82746f651bfaeff11daf0ae07b437e8a083
berezhnoy <berezhnoy@5079a5ec-52b0-490e-b3bd-9f0da412a43b>
2012-09-28 11:07:29 +0000

    -
    git-svn-id: svn+ssh://195.201.30.214/var/svn-repos/lazaProj/branches/berBranch/trunk@503 5079a5ec-52b0-490e-b3bd-9f0da412a43b

--- libs/ringpix.cpp
+++ libs/ringpix.cpp
@@ -204,3 +204,226 @@ void pix2ang_ring( long nside, long ipix, double *theta, double *phi) {
     *phi   = (1.*iphi - 0.5) * PI/(2.*iring);
   }
 }
+
+void ang2pix_nest( const long nside, double theta, double phi, long *ipix) {
+
+  /* =======================================================================
+   * subroutine ang2pix_nest(nside, theta, phi, ipix)
+   * =======================================================================
+   * gives the pixel number ipix (NESTED) corresponding to angles theta and phi
+   *
+   * the computation is made to the highest resolution available (nside=8192)
+   * and then degraded to that required (by integer division)
+   * this doesn't cost more, and it makes sure that the treatement of round-off
+   * will be consistent for every resolution
+   * =======================================================================
+   */
+
+  double z, za, z0, tt, tp, tmp;
+  int    face_num,jp,jm;
+  long   ifp, ifm;
+  int    ix, iy, ix_low, ix_hi, iy_low, iy_hi, ipf, ntt;
+  double piover2 = 0.5*M_PI, pi = M_PI, twopi = 2.0*M_PI;
+  int    ns_max = 8192;
+  static int x2pix[128], y2pix[128];
+  static char setup_done = 0;
+
+  if( nside<1 || nside>ns_max ) {
+    fprintf(stderr, "%s (%d): nside out of range: %ld\n", __FILE__, __LINE__, nside);
+    exit(0);
+  }
+  if( theta<0. || theta>pi ) {
+    fprintf(stderr, "%s (%d): theta out of range: %f\n", __FILE__, __LINE__, theta);
+    exit(0);
+  }
+  if( !setup_done ) {
+    mk_xy2pix(x2pix,y2pix);
+    setup_done = 1;
+  }
+
+  z  = cos(theta);
+  za = fabs(z);
+  z0 = 2./3.;
+  if( phi>=twopi ) phi = phi - twopi;
+  if( phi<0. )    phi = phi + twopi;
+  tt = phi / piover2; /* in [0,4[ */
+
+  if( za<=z0 ) { /* equatorial region */
+
+    /* (the index of edge lines increase when the longitude=phi goes up) */
+    jp = (int)floor(ns_max*(0.5 + tt - z*0.75)); /* ascending edge line index */
+    jm = (int)floor(ns_max*(0.5 + tt + z*0.75)); /* descending edge line index */
+
+    /* finds the face */
+    ifp = jp / ns_max; /* in {0,4} */
+    ifm = jm / ns_max;
+
+    if( ifp==ifm ) face_num = (int)fmod(ifp,4) + 4; /* faces 4 to 7 */
+    else if( ifp<ifm ) face_num = (int)fmod(ifp,4); /* (half-)faces 0 to 3 */
+    else face_num = (int)fmod(ifm,4) + 8;           /* (half-)faces 8 to 11 */
+
+    ix = (int)fmod(jm, ns_max);
+    iy = ns_max - (int)fmod(jp, ns_max) - 1;
+  }
+  else { /* polar region, za > 2/3 */
+
+    ntt = (int)floor(tt);
+    if( ntt>=4 ) ntt = 3;
+    tp = tt - ntt;
+    tmp = sqrt( 3.*(1. - za) ); /* in ]0,1] */
+
+    /* (the index of edge lines increase when distance from the closest pole
+     * goes up)
+     */
+    /* line going toward the pole as phi increases */
+    jp = (int)floor( ns_max * tp          * tmp );
+
+    /* that one goes away of the closest pole */
+    jm = (int)floor( ns_max * (1. - tp) * tmp );
+    jp = (int)(jp < ns_max-1 ? jp : ns_max-1);
+    jm = (int)(jm < ns_max-1 ? jm : ns_max-1);
+
+    /* finds the face and pixel's (x,y) */
+    if( z>=0 ) {
+      face_num = ntt; /* in {0,3} */
+      ix = ns_max - jm - 1;
+      iy = ns_max - jp - 1;
+    }
+    else {
+      face_num = ntt + 8; /* in {8,11} */
+      ix =  jp;
+      iy =  jm;
+    }
+  }
+
+  ix_low = (int)fmod(ix,128);
+  ix_hi  =     ix/128;
+  iy_low = (int)fmod(iy,128);
+  iy_hi  =     iy/128;
+
+  ipf = (x2pix[ix_hi]+y2pix[iy_hi]) * (128 * 128)+ (x2pix[ix_low]+y2pix[iy_low]);
+  ipf = (long)(ipf / pow(ns_max/nside,2));     /* in {0, nside**2 - 1} */
+  *ipix =(long)( ipf + face_num*pow(nside,2)); /* in {0, 12*nside**2 - 1} */
+}
+
+void pix2ang_nest( long nside, long ipix, double *theta, double *phi) {
+
+  /*
+    c=======================================================================
+    subroutine pix2ang_nest(nside, ipix, theta, phi)
+    c=======================================================================
+    c     gives theta and phi corresponding to pixel ipix (NESTED)
+    c     for a parameter nside
+    c=======================================================================
+  */
+
+      int npix, npface, face_num;
+      int  ipf, ip_low, ip_trunc, ip_med, ip_hi;
+      int     ix, iy, jrt, jr, nr, jpt, jp, kshift, nl4;
+      double z, fn, fact1, fact2;
+      double piover2=0.5*M_PI;
+      int ns_max=8192;
+
+      static int pix2x[1024], pix2y[1024];
+      //      common /pix2xy/ pix2x, pix2y
+
+      int jrll[12], jpll[12];// ! coordinate of the lowest corner of each face
+      //      data jrll/2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4/ ! in unit of nside
+      //      data jpll/1, 3, 5, 7, 0, 2, 4, 6, 1, 3, 5, 7/ ! in unit of nside/2
+      jrll[0]=2;
+      jrll[1]=2;
+      jrll[2]=2;
+      jrll[3]=2;
+      jrll[4]=3;
+      jrll[5]=3;
+      jrll[6]=3;
+      jrll[7]=3;
+      jrll[8]=4;
+      jrll[9]=4;
+      jrll[10]=4;
+      jrll[11]=4;
+      jpll[0]=1;
+      jpll[1]=3;
+      jpll[2]=5;
+      jpll[3]=7;
+      jpll[4]=0;
+      jpll[5]=2;
+      jpll[6]=4;
+      jpll[7]=6;
+      jpll[8]=1;
+      jpll[9]=3;
+      jpll[10]=5;
+      jpll[11]=7;
+
+
+      if( nside<1 || nside>ns_max ) {
+    fprintf(stderr, "%s (%d): nside out of range: %ld\n", __FILE__, __LINE__, nside);
+    exit(0);
+      }
+      npix = 12 * nside*nside;
+      if( ipix<0 || ipix>npix-1 ) {
+    fprintf(stderr, "%s (%d): ipix out of range: %ld\n", __FILE__, __LINE__, ipix);
+    exit(0);
+      }
+
+      /* initiates the array for the pixel number -> (x,y) mapping */
+      if( pix2x[1023]<=0 ) mk_pix2xy(pix2x,pix2y);
+
+      fn = 1.*nside;
+      fact1 = 1./(3.*fn*fn);
+      fact2 = 2./(3.*fn);
+      nl4   = 4*nside;
+
+      //c     finds the face, and the number in the face
+      npface = nside*nside;
+
+      face_num = ipix/npface;//  ! face number in {0,11}
+      ipf = (int)fmod(ipix,npface);//  ! pixel number in the face {0,npface-1}
+
+      //c     finds the x,y on the face (starting from the lowest corner)
+      //c     from the pixel number
+      ip_low = (int)fmod(ipf,1024);//       ! content of the last 10 bits
+      ip_trunc =   ipf/1024 ;//       ! truncation of the last 10 bits
+      ip_med = (int)fmod(ip_trunc,1024);//  ! content of the next 10 bits
+      ip_hi  =     ip_trunc/1024   ;//! content of the high weight 10 bits
+
+      ix = 1024*pix2x[ip_hi] + 32*pix2x[ip_med] + pix2x[ip_low];
+      iy = 1024*pix2y[ip_hi] + 32*pix2y[ip_med] + pix2y[ip_low];
+
+      //c     transforms this in (horizontal, vertical) coordinates
+      jrt = ix + iy;//  ! 'vertical' in {0,2*(nside-1)}
+      jpt = ix - iy;//  ! 'horizontal' in {-nside+1,nside-1}
+
+      //c     computes the z coordinate on the sphere
+      //      jr =  jrll[face_num+1]*nside - jrt - 1;//   ! ring number in {1,4*nside-1}
+      jr =  jrll[face_num]*nside - jrt - 1;
+      //      cout << "face_num=" << face_num << endl;
+      //      cout << "jr = " << jr << endl;
+      //      cout << "jrll(face_num)=" << jrll[face_num] << endl;
+      //      cout << "----------------------------------------------------" << endl;
+      nr = nside;//                  ! equatorial region (the most frequent)
+      z  = (2*nside-jr)*fact2;
+      kshift = (int)fmod(jr - nside, 2);
+      if( jr<nside ) { //then     ! north pole region
+         nr = jr;
+         z = 1. - nr*nr*fact1;
+         kshift = 0;
+      }
+      else {
+    if( jr>3*nside ) {// then ! south pole region
+         nr = nl4 - jr;
+         z = - 1. + nr*nr*fact1;
+         kshift = 0;
+    }
+      }
+      *theta = acos(z);
+
+      //c     computes the phi coordinate on the sphere, in [0,2Pi]
+      //      jp = (jpll[face_num+1]*nr + jpt + 1 + kshift)/2;//  ! 'phi' number in the ring in {1,4*nr}
+      jp = (jpll[face_num]*nr + jpt + 1 + kshift)/2;
+      if( jp>nl4 ) jp = jp - nl4;
+      if( jp<1 )   jp = jp + nl4;
+
+      *phi = (jp - (kshift+1)*0.5) * (piover2 / nr);
+
+}
--- libs/ringpix.h
+++ libs/ringpix.h
@@ -4,4 +4,7 @@
 void pix2ang_ring( long nside, long ipix, double *theta, double *phi);
 void ang2pix_ring( const long nside, double theta, double phi, long *ipix);
 
+void ang2pix_nest(const long nside, double theta, double phi, long *ipix);
+void pix2ang_nest(long nside, long ipix, double *theta, double *phi)
+
 #endif // RINGPIX_H
