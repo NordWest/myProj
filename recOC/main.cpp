59a52fb7da29341133bf37afab39859105d68ce4
Berezhnoy <berezhnoyaa@gmail.com>
2013-02-05 13:16:25 +0400

    mercury 6 дал хороший результат на Ceres'е

--- recOC/main.cpp
+++ recOC/main.cpp
@@ -45,6 +45,7 @@ int main(int argc, char *argv[])
     nbody = new dele();
     int centr_num;
     int status;
+    int plaNum;
     if(useEPM)
     {
         status = !InitTxt(epmDir.toAscii().data());
@@ -61,13 +62,16 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    QString inFileName(argv[1]);
+    QFileInfo fi(inFileName);
+    QString dxFileName = QString("%1_dr.txt").arg(fi.absoluteFilePath());
 
-    QString inFileName("small.log");
+    //QString inFileName("small.log");
     QFile inFile(inFileName);
     inFile.open(QFile::ReadOnly);
     QTextStream inStm(&inFile);
 
-    QString dxFileName("small_dr.txt");
+    //QString dxFileName("small_dr.txt");
     QFile dxFile(dxFileName);
     dxFile.open(QFile::WriteOnly | QFile::Truncate);
     QTextStream dxStm(&dxFile);
@@ -76,17 +80,21 @@ int main(int argc, char *argv[])
     {
         tStr = inStm.readLine();
         dataSL = tStr.split("|");
-        if(dataSL.size()<8) continue;
+        if(dataSL.size()<10) continue;
 
         time = dataSL.at(0).toDouble();
-        name = dataSL.at(1).simplified();
 
-        X[0] = dataSL.at(2).toDouble();
-        X[1] = dataSL.at(3).toDouble();
-        X[2] = dataSL.at(4).toDouble();
+
+        X[0] = dataSL.at(1).toDouble();
+        X[1] = dataSL.at(2).toDouble();
+        X[2] = dataSL.at(3).toDouble();
         V[0] = dataSL.at(5).toDouble();
         V[1] = dataSL.at(6).toDouble();
         V[2] = dataSL.at(7).toDouble();
+
+        name = dataSL.at(8).simplified();
+
+        qDebug() << QString("X: %1\t%2\t%3\nV: %4\t%5\t%6\n").arg(X[0]).arg(X[1]).arg(X[2]).arg(V[0]).arg(V[1]).arg(V[2]);
 
 /*
         if(useEPM)
@@ -155,10 +163,16 @@ int main(int argc, char *argv[])
         outerArguments.clear();
 
         TDB2UTC(time, &jdUTC);
-        outerArguments << QString("-name=\"%1\"").arg(name.simplified());
+        outerArguments << QString("-name=%1").arg(name.simplified().toLower());
         sJD = QString("%1").arg(jdUTC, 15, 'f',7);
         outerArguments << QString("-ep=%1").arg(sJD);
-        outerArguments << "-type=aster";
+
+        if(useEPM) plaNum = epm_planet_num(name);
+        else plaNum = planet_num(name.toAscii().data());
+        if(plaNum!=-1) outerArguments << "-type=planet";
+        else outerArguments << "-type=aster";
+
+
         outerArguments << QString("-observer=@sun");
         //outerArguments << "-tcoor=2";
         //outerArguments << "-rplane=1";
@@ -192,14 +206,30 @@ int main(int argc, char *argv[])
             if(objDataStr.at(0)=='#') continue;
 
             resSL =  objDataStr.split(" ", QString::SkipEmptyParts);
-            if(resSL.size()<10) continue;
+            if(resSL.size()<8) continue;
             isObj = 1;
             X0[0] = resSL.at(1).toDouble();
             X0[1] = resSL.at(2).toDouble();
             X0[2] = resSL.at(3).toDouble();
-            V0[0] = resSL.at(8).toDouble();
-            V0[1] = resSL.at(9).toDouble();
-            V0[2] = resSL.at(10).toDouble();
+            V0[0] = resSL.at(5).toDouble();
+            V0[1] = resSL.at(6).toDouble();
+            V0[2] = resSL.at(7).toDouble();
+            qDebug() << QString("X0: %1\t%2\t%3\nV0: %4\t%5\t%6\n").arg(X0[0]).arg(X0[1]).arg(X0[2]).arg(V0[0]).arg(V0[1]).arg(V0[2]);
+
+            dX[0] = X[0] - X0[0];
+            dX[1] = X[1] - X0[1];
+            dX[2] = X[2] - X0[2];
+
+            dist = sqrt(dX[0]*dX[0] + dX[1]*dX[1] + dX[2]*dX[2]);
+
+            dV[0] = V[0] - V0[0];
+            dV[1] = V[1] - V0[1];
+            dV[2] = V[2] - V0[2];
+
+            dvel = sqrt(dV[0]*dV[0] + dV[1]*dV[1] + dV[2]*dV[2]);
+
+            dxStm << QString("%1|%2|%3|%4|%5|%6|%7|%8|%9|%10\n").arg(time).arg(name).arg(dX[0]).arg(dX[1]).arg(dX[2]).arg(dist).arg(dV[0]).arg(dV[1]).arg(dV[2]).arg(dvel);
+            qDebug() << QString("%1|%2|%3|%4|%5|%6|%7|%8|%9|%10\n").arg(time).arg(name).arg(dX[0]).arg(dX[1]).arg(dX[2]).arg(dist).arg(dV[0]).arg(dV[1]).arg(dV[2]).arg(dvel);
 /*
             X0[0] += XE0[0];
             X0[1] += XE0[1];
@@ -226,20 +256,7 @@ int main(int argc, char *argv[])
 
         }
 
-        dX[0] = X[0] - X0[0];
-        dX[1] = X[1] - X0[1];
-        dX[2] = X[2] - X0[2];
 
-        dist = sqrt(dX[0]*dX[0] + dX[1]*dX[1] + dX[2]*dX[2]);
-
-        dV[0] = V[0] - V0[0];
-        dV[1] = V[1] - V0[1];
-        dV[2] = V[2] - V0[2];
-
-        dvel = sqrt(dV[0]*dV[0] + dV[1]*dV[1] + dV[2]*dV[2]);
-
-        dxStm << QString("%1|%2|%3|%4|%5|%6|%7|%8|%9|%10\n").arg(time).arg(name).arg(dX[0]).arg(dX[1]).arg(dX[2]).arg(dist).arg(dV[0]).arg(dV[1]).arg(dV[2]).arg(dvel);
-        qDebug() << QString("%1|%2|%3|%4|%5|%6|%7|%8|%9|%10\n").arg(time).arg(name).arg(dX[0]).arg(dX[1]).arg(dX[2]).arg(dist).arg(dV[0]).arg(dV[1]).arg(dV[2]).arg(dvel);
 
     }
 
