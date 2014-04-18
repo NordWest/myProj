c0b8b1bad231f111726a5b2fdb2f080c2bd89583
Berezhnoy <berezhnoyaa@gmail.com>
2014-04-18 14:17:48 +0400

    -

--- libs/observ.cpp
+++ libs/observ.cpp
@@ -235,6 +235,110 @@ int observ::det_vect_radec(double *state2sun, double *raRad, double *decRad, dou
     V[1] = state2sun[4];
     V[2] = state2sun[5];
 
+//sun?
+    XE0[0] = state[0];
+    XE0[1] = state[1];
+    XE0[2] = state[2];
+
+
+    if(this->place->detR(&XS0[0], &XS0[1], &XS0[2], ctime.TDB(), SUN_NUM, 0, 0, 0)) return 1;
+    if(this->place->detR(&VS0[0], &VS0[1], &VS0[2], ctime.TDB(), SUN_NUM, 1, 0, 0)) return 1;
+
+//X1 - ssb
+    X1[0] = X[0] + XS0[0];
+    X1[1] = X[1] + XS0[1];
+    X1[2] = X[2] + XS0[2];
+
+    V1[0] = V[0] + VS0[0];
+    V1[1] = V[1] + VS0[1];
+    V1[2] = V[2] + VS0[2];
+
+    XEB0[0] = XE0[0] + XS0[0];
+    XEB0[1] = XE0[1] + XS0[1];
+    XEB0[2] = XE0[2] + XS0[2];
+
+
+    R[0] = X[0] - XE0[0];
+    R[1] = X[1] - XE0[1];
+    R[2] = X[2] - XE0[2];
+
+    normR = norm3(R);
+
+//    if(corr)
+//    {
+        normE = norm3(XE0);
+        normP = normR;
+        normQ = norm3(X);
+
+        ct1 = normP;
+
+        do
+        {
+            ct0 = ct1;
+            tau = ct1/CAU;
+
+            Qb[0] = X1[0] - V1[0]*tau;
+            Qb[1] = X1[1] - V1[1]*tau;
+            Qb[2] = X1[2] - V1[2]*tau;
+
+            P[0] = Qb[0] - XEB0[0];
+            P[1] = Qb[1] - XEB0[1];
+            P[2] = Qb[2] - XEB0[2];
+
+            normP = norm3(P);
+
+            Q[0] = Qb[0] - XS0[0];
+            Q[1] = Qb[1] - XS0[1];
+            Q[2] = Qb[2] - XS0[2];
+
+            normQ = norm3(Q);
+
+            ct1 = normP+2.0*muc2*log((normE+normQ+normP)/(normE+normQ-normP));
+
+        }while((fabs(ct1-ct0)/fabs(ct1))>1e-12);
+//    }
+
+    rdsys(raRad, decRad, P[0], P[1], P[2]);
+
+    if(range!=NULL)
+    {
+        range[0] = R[0];
+        range[1] = R[1];
+        range[2] = R[2];
+    }
+}
+/*
+int observ::det_vect_radec_sun(double *state2sun, double *raRad, double *decRad, double *range, int corr)
+{
+    double *R, *X, *V, *X1, *V1, *XE0, *XEB0, *XS0, *VS0, *Q, *Qb, *P;
+    R = new double[3];
+    P = new double[3];
+    X = new double[3];
+    V = new double[3];
+    X1 = new double[3];
+    V1 = new double[3];
+    XE0 = new double[3];
+    XEB0 = new double[3];
+    XS0 = new double[3];
+    VS0 = new double[3];
+    Q = new double[3];
+    Qb = new double[3];
+
+    double normR, normE, normP, normQ;
+    double ct0, ct1, tau, muc2;
+
+    muc2 = 9.8704e-9;
+
+
+//X - sun
+    X[0] = state2sun[0];
+    X[1] = state2sun[1];
+    X[2] = state2sun[2];
+
+    V[0] = state2sun[3];
+    V[1] = state2sun[4];
+    V[2] = state2sun[5];
+
 //ssb
     XE0[0] = state[0];
     XE0[1] = state[1];
@@ -257,7 +361,7 @@ int observ::det_vect_radec(double *state2sun, double *raRad, double *decRad, dou
     XEB0[0] = XE0[0] + XS0[0];
     XEB0[1] = XE0[1] + XS0[1];
     XEB0[2] = XE0[2] + XS0[2];
-*/
+/
 
     R[0] = X1[0] - XE0[0];
     R[1] = X1[1] - XE0[1];
@@ -291,7 +395,7 @@ int observ::det_vect_radec(double *state2sun, double *raRad, double *decRad, dou
             Q[0] = Qb[0] - XS0[0];
             Q[1] = Qb[1] - XS0[1];
             Q[2] = Qb[2] - XS0[2];
-*/
+/
             normQ = norm3(Q);
 
             ct1 = normP+2.0*muc2*log((normE+normQ+normP)/(normE+normQ-normP));
@@ -309,7 +413,7 @@ int observ::det_vect_radec(double *state2sun, double *raRad, double *decRad, dou
         range[2] = R[2];
     }
 }
-
+*/
 int observ::radec500(double *raRad0, double *decRad0, double raRad, double decRad, double ro)
 {
     double X[3];
